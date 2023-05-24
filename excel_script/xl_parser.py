9f6183dd84a6b56ece63bbbaa99a5487e69d7045
Ilyazanuda <ilyazanuda@gmail.com>
2023-05-24 11:47:56 +0300

    Almost ready excel

--- excel_script/xl_parser.py
+++ excel_script/xl_parser.py
@@ -18,7 +18,7 @@ def df_to_csv(df, destination, selected_columns):
     clear_df = df[df['valid'].astype(bool) == True]
     clear_df[selected_columns].to_csv(f"{destination}.csv", index=False, header=True)
     bad_df = df[df['valid'].astype(bool) == False]
-    bad_df[selected_columns].to_csv(f"{destination}99.csv", index=False, header=True)
+    bad_df[selected_columns].to_csv(f"{destination}_bad.csv", index=False, header=True)
 
 
 def add_additional_info(row, sep):
@@ -36,8 +36,14 @@ def normalize_phone(row):
 
 
 def validation_process(row):
-    print(str(row['Mobile number']))
-    pass
+    pattern_ssn = r'^(?:\d[-.]?){2}\d[-.]?(?:\d[-.]?){4}\d[-.]?\d$'
+    pattern_name = r"^(?!.*[A-Z]{3})(?!.*[A-Z].*[A-Z].*[A-Z])(?:[A-Z][a-z']* ?)+$"
+    pattern_mobile = r'^[1]?\d{10}$'
+    ssn = bool(re.match(pattern_ssn, row['SSN']))
+    first_name = bool(re.match(pattern_name, row['First Name']))
+    last_name = bool(re.match(pattern_name, row['Last Name']))
+    mobile = bool(re.match(pattern_mobile, re.sub(r'\(|\)|-|\.|\s', '', row['Mobile number'])))
+    return all([ssn, first_name, last_name, mobile])
 
 
 def normalize_address(row):
@@ -89,7 +95,8 @@ def main():
     source, destination = get_args().source, get_args().destination
 
     if not source:
-        source = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'excel', 'data.xlsx')
+        source = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+                              'excel', 'data.xlsx')
 
     if destination:
         destination = os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -104,8 +111,6 @@ def main():
             sep = '|'
             sheet_name = pd.ExcelFile(source).sheet_names[0]
             df = pd.read_excel(io=source, sheet_name=sheet_name)
-            print(df.columns)
-
             processing(df, destination, sep)
 
             print(f'CSV is ready. Path: {destination + ".csv"}')
